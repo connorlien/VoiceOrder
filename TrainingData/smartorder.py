4d4d2b60805f6633f7ee598db3466e53143ef7e4
Connor <connorlien19@gmail.com>
2020-01-11 13:29:19 -0800

    training data round 3

--- TrainingData/smartorder.py
+++ TrainingData/smartorder.py
@@ -1,8 +1,8 @@
 import json
 
-reader = open("training_data_2.txt", "r")
+reader = open("training_data_3.txt", "r")
 data = [i[:-1] for i in reader.readlines()]
 
 for i in range(len(data)):
-    writer = open("file" + str(i+200) + ".jsonl", "w+", encoding='utf-8')
+    writer = open("file" + str(i+300) + ".jsonl", "w+", encoding='utf-8')
     writer.write(json.dumps({"text_snippet":{"content": data[i].strip()}}, ensure_ascii=False))
