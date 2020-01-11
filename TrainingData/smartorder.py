92c222da74e2e999239f1da0d31b5052c1650766
Matthew Park <42101816+matthewpark@users.noreply.github.com>
2020-01-11 12:27:10 -0800

    new training data
    roses are red
    poems are nice
    making this project
    is like walking on ice

--- TrainingData/smartorder.py
+++ TrainingData/smartorder.py
@@ -1,8 +1,8 @@
 import json
 
-reader = open("training_data.txt", "r")
+reader = open("training_data_2.txt", "r")
 data = [i[:-1] for i in reader.readlines()]
 
 for i in range(len(data)):
-    writer = open("file" + str(i) + ".jsonl", "w+", encoding='utf-8')
+    writer = open("file" + str(i+200) + ".jsonl", "w+", encoding='utf-8')
     writer.write(json.dumps({"text_snippet":{"content": data[i].strip()}}, ensure_ascii=False))
