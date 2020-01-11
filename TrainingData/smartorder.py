6856f8bd7ad17e9e209c06bd4014eef8755ee30d
Connor <connorlien19@gmail.com>
2020-01-10 21:05:04 -0800

    skeleton for training data

---
+++ TrainingData/smartorder.py
@@ -0,0 +1,8 @@
+import json
+
+reader = open("training_data.txt", "r")
+data = [i[:-1] for i in reader.readlines()]
+
+for i in range(len(data)):
+    writer = open("file" + str(i) + ".jsonl", "w+", encoding='utf-8')
+    writer.write(json.dumps({"text_snippet":{"content": data[i].strip()}}, ensure_ascii=False))
