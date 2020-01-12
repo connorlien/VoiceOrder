1ad0e28ff70de0c3b469bacb14650f2bdd50928f
Connor <connorlien19@gmail.com>
2020-01-11 17:58:39 -0800

    fleshed out code for postprocessing

--- postprocessing.py
+++ postprocessing.py
@@ -1,102 +1,110 @@
+from boberMenuTime import *
 
-def process_order(order):
+def process_order(order): # order is a dictionary in JSON form
+
+    # collect data
     tags = order["payload"]
+
+    # set default values
     result = {
         "order_name": "",
         "amount_of_drinks": 1,
         "drink_size": "regular",
         "ice": 100,
         "sweetness": 100,
-        "toppings": [],
+        "topping": [],
+        "price": 0.00
     }
+
+    # read from the order
     for tag in tags:
-        result[tag["displayName"]] = tag["textExtraction"]["textSegment"]["content"]
-    result["amount_of_drinks"] = int(result["amount_of_drinks"])
+        if tag["displayName"] == "topping":
+            result["topping"].append(tag["textExtraction"]["textSegment"]["content"])
+        else:
+            result[tag["displayName"]] = tag["textExtraction"]["textSegment"]["content"]
+
+    #custom processing
+    if result["amount_of_drinks"] == "a":
+        result["amount_of_drinks"] = 1
+    else:
+        result["amount_of_drinks"] = int(result["amount_of_drinks"])
+    result["price"] = calculate_price(result)
+
     return result
+
+
+def calculate_price(order):
+
+    # load data
+    menu = get_menu()
+    toppings_menu = get_toppings()
+
+    # calculate drink and toppings prices
+    drink_price = menu[order["amount_of_drinks"]][order["drink_size"]] * order["amount_of_drinks"]
+    toppings_price = sum([toppings_menu[i] for i in order["topping"]])
+
+    return drink_price + toppings_price
+
 
 test = {
   "payload": [
     {
-      "annotationSpecId": "2333535309070860288",
+      "annotationSpecId": "705695150008958976",
+      "displayName": "additive_adjective",
+      "textExtraction": {
+        "score": 0.99953187,
+        "textSegment": {
+          "startOffset": "6",
+          "endOffset": "11",
+          "content": "order"
+        }
+      }
+    },
+    {
+      "annotationSpecId": "1491432547496755200",
+      "displayName": "amount_of_drinks",
+      "textExtraction": {
+        "score": 0.99947494,
+        "textSegment": {
+          "startOffset": "12",
+          "endOffset": "13",
+          "content": "a"
+        }
+      }
+    },
+    {
+      "annotationSpecId": "5878079322043973632",
+      "displayName": "drink_size",
+      "textExtraction": {
+        "score": 0.99950624,
+        "textSegment": {
+          "startOffset": "14",
+          "endOffset": "19",
+          "content": "large"
+        }
+      }
+    },
+    {
+      "annotationSpecId": "702317450288431104",
       "displayName": "order_name",
       "textExtraction": {
-        "score": 0.99900645,
+        "score": 0.999563,
         "textSegment": {
-          "startOffset": "62",
-          "endOffset": "85",
-          "content": "black milk tea"
+          "startOffset": "20",
+          "endOffset": "30",
+          "content": "black milk"
         }
       }
     },
     {
-      "annotationSpecId": "1180613804464013312",
+      "annotationSpecId": "1398827280158949376",
       "displayName": "sweetness",
       "textExtraction": {
-        "score": 0.9992172,
+        "score": 0.9994066,
         "textSegment": {
-          "startOffset": "90",
-          "endOffset": "117",
-          "content": "developmental abnormalities"
-        }
-      }
-    },
-    {
-      "annotationSpecId": "2333535309070860288",
-      "displayName": "SpecificDisease",
-      "textExtraction": {
-        "score": 0.999485,
-        "textSegment": {
-          "startOffset": "127",
-          "endOffset": "159",
-          "content": "Familial hypobetalipoproteinemia"
-        }
-      }
-    },
-    {
-      "annotationSpecId": "1180613804464013312",
-      "displayName": "DiseaseClass",
-      "textExtraction": {
-        "score": 0.9991241,
-        "textSegment": {
-          "startOffset": "166",
-          "endOffset": "195",
-          "content": "autosomal codominant disorder"
-        }
-      }
-    },
-    {
-      "annotationSpecId": "2333535309070860288",
-      "displayName": "SpecificDisease",
-      "textExtraction": {
-        "score": 0.9990812,
-        "textSegment": {
-          "startOffset": "346",
-          "endOffset": "369",
-          "content": "hypobetalipoproteinemia"
-        }
-      }
-    },
-    {
-      "annotationSpecId": "1180613804464013312",
-      "displayName": "DiseaseClass",
-      "textExtraction": {
-        "score": 0.9863149,
-        "textSegment": {
-          "startOffset": "428",
-          "endOffset": "453",
-          "content": "coronary vascular disease"
-        }
-      }
-    },
-    {
-      "annotationSpecId": "2333535309070860288",
-      "displayName": "SpecificDisease",
-      "textExtraction": {
-        "score": 0.9989736,
-        "textSegment": {
-          "startOffset": "597",
-          "endOffset": "620",
-          "content": "hypobetalipoproteinemia"
+          "startOffset": "40",
+          "endOffset": "45",
+          "content": "fifty"
         }
       }
     }
